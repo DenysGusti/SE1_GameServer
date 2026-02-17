6677a2a09f42f01e54da1cce340880d91b9ea076
Denys Gusti <denys.gusti65@gmail.com>
2026-02-17 02:53:30 +0100

    add repositories

---
+++ src/main/java/server/repositories/PlayerRepository.java
@@ -0,0 +1,14 @@
+package server.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import server.entities.PlayerEntity;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<PlayerEntity, String> {
+    @Modifying
+    @Query("DELETE FROM PlayerEntity p WHERE NOT EXISTS (SELECT 1 FROM PlayerParticipationEntity pp WHERE pp.player = p)")
+    void deleteOrphanedPlayers();
+}
