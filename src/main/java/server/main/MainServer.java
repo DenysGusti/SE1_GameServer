8c8d07ea85129131cd74f2dec6ccfefa9edbe7be
Denys Gusti <denys.gusti65@gmail.com>
2026-02-22 04:21:28 +0100

    rename package names

--- src/main/java/server/main/MainServer.java
+++ src/main/java/server/main/MainServer.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -18,8 +17,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * and also, relevant for us, controllers holding endpoints 
  */
 @SpringBootApplication(scanBasePackages = "server")
-@EnableJpaRepositories(basePackages = "server.repositories")
-@EntityScan(basePackages = "server.entities")
+@EnableJpaRepositories(basePackages = "server.repository")
+@EntityScan(basePackages = "server.entity")
 @Configuration
 public class MainServer {
 
