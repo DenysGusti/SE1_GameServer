bbb2c2289b21aca37db1caf894a90107b4e71b6a
Denys Gusti <denys.gusti65@gmail.com>
2026-02-25 15:54:03 +0100

    remove exception

--- src/main/java/server/exception/TooFastPollingException.java
+++
@@ -1,8 +0,0 @@
-package server.exception;
-
-public class TooFastPollingException extends GenericServerException {
-    public TooFastPollingException() {
-        super("A client requested the game state too quickly. You must wait at least 400 milliseconds (0.4 seconds) " +
-                "between consecutive game state requests.");
-    }
-}
