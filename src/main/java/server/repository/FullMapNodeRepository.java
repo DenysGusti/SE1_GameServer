9c1b6700d3756b5b21ecf169b8b3fadd035919dc
Denys Gusti <denys.gusti65@gmail.com>
2026-03-07 03:12:23 +0100

    add findFirstByGameIdAndXAndY

--- src/main/java/server/repository/FullMapNodeRepository.java
+++ src/main/java/server/repository/FullMapNodeRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import server.entity.FullMapNodeEntity;
 
+import java.util.Optional;
+
 @Repository
 public interface FullMapNodeRepository extends JpaRepository<FullMapNodeEntity, Long> {
+    Optional<FullMapNodeEntity> findFirstByGameIdAndXAndY(String gameId, int x, int y);
 }
